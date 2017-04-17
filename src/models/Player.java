a151644307024e04ed84b63b272ebf00316eb306
Kriztoper Urmeneta <kriztoperurmeneta@gmail.com>
2017-04-17 14:59:01 +0800

    Added resources folder for images

---
+++ src/models/Player.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Player {
+
+	private String playerName;
+	private int x;
+	private int y;
+		
+	
+	public Player() {
+		loadImages();
+	}
+	
+	public void loadImages() {
+		
+	}
+	
+	/* the method must return a boolean value
+	   to determine if the player was able to jump
+	   true for jump succes, false for jump failed. */
+	public boolean jump() {
+		//TODO: player must virtually jump using sprite
+		
+		return false;
+	}
+	
+	/* same with jump the boolean value returned 
+	   determines if the punch execution was a success. */
+	public boolean punch() {
+		//TODO: player must virtually punch using sprite
+		return false;
+	}
+}
