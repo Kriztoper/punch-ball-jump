95da244d69a39fcf5c43679bea4173eecd6e8efe
Kristine Joyce Sabar <kjsabar@gmail.com>
2018-07-19 12:47:27 +0800

    updated MenuPanel and powerups' images

--- src/views/MenuPanel.java
+++ src/views/MenuPanel.java
@@ -1,7 +1,15 @@
 package views;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
 
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -14,8 +22,10 @@ public class MenuPanel extends JPanel implements Commons {
 	private JButton creditsButton;
 	private JButton onePlayerButton;
 	private JButton twoPlayersButton;
+	private Image bg;
 
 	public MenuPanel() {
+		bg = Toolkit.getDefaultToolkit().createImage("images/MenuPanel.png");;
 		initPanel();
 		initComponents();
 		addComponents();
@@ -23,19 +33,44 @@ public class MenuPanel extends JPanel implements Commons {
 
 	public void initPanel() {
 		setLayout(null);
-		setBackground(new Color(100, 149, 237)); // TODO: change with new edited menu image bg
+		setOpaque(false);
 		setSize(Commons.WIDTH, Commons.HEIGHT);
+		setVisible(true);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    g.drawImage(bg, 0, 0, this);
+	}
+	
+	public void modifyButton(JButton button) {
+		button.setOpaque(false);
+		button.setContentAreaFilled(false);
+		button.setBorderPainted(false);
+		button.setFocusable(false);
 	}
 
 	public void initComponents() {
-		onePlayerButton = new JButton("One Player");
-		twoPlayersButton = new JButton("Two Players");
-		howToPlayButton = new JButton("How to Play");
-		creditsButton = new JButton("Credits");
-		onePlayerButton.setBounds(Constants.WIDTH / 2 - 100, 100, 200, 60);
-		twoPlayersButton.setBounds(Constants.WIDTH / 2 - 100, 160, 200, 60);
-		howToPlayButton.setBounds(Constants.WIDTH / 2 - 100, 220, 200, 60);
-		creditsButton.setBounds(Constants.WIDTH / 2 - 100, 280, 200, 60);
+		onePlayerButton = new JButton(new ImageIcon("images/1player_button.png"));
+		onePlayerButton.setBounds(100, 410, 210, 60);
+		onePlayerButton.setRolloverIcon(new ImageIcon("images/1player_hoverButton.png"));
+		modifyButton(onePlayerButton);
+
+		twoPlayersButton = new JButton(new ImageIcon("images/2players_button.png"));
+		twoPlayersButton.setBounds(330, 410, 210, 60);
+		twoPlayersButton.setRolloverIcon(new ImageIcon("images/2players_hoverButton.png"));
+		modifyButton(twoPlayersButton);
+		
+		howToPlayButton = new JButton(new ImageIcon("images/help_button.png"));
+		howToPlayButton.setBounds(100, 470, 210, 60);
+		howToPlayButton.setRolloverIcon(new ImageIcon("images/help_hoverButton.png"));
+		modifyButton(howToPlayButton);
+		
+		creditsButton = new JButton(new ImageIcon("images/credits_button.png"));
+		creditsButton.setBounds(330, 470, 210, 60);
+		creditsButton.setRolloverIcon(new ImageIcon("images/credits_hoverButton.png"));
+		modifyButton(creditsButton);
 	}
 
 	public void addComponents() {
