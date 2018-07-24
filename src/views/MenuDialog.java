3b34d61569942a51878e1a89133c6f92b0955eca
chardi <balenarichard@gmail.com>
2018-07-24 15:20:32 +0800

    Menu Dialog buttons modified

--- src/views/MenuDialog.java
+++ src/views/MenuDialog.java
@@ -1,7 +1,11 @@
 package views;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -11,21 +15,41 @@ public class MenuDialog extends JDialog {
 	private JLabel jLabel;
 	private JButton yesButton;
 	private JButton noButton;
-
+	
 	public MenuDialog() {
 		setUndecorated(true);
-		setLayout(new FlowLayout());
 		setSize(200, 200);
 		setLocationRelativeTo(null);
 		setVisible(false);
+		setBackground(new Color(8,31,39));
+		
 		jLabel = new JLabel("Return to Main Menu?");
-		yesButton = new JButton("YES");
+		jLabel.setBounds(24, 63, 151, 21);
+		jLabel.setFont(new Font("Dialog", Font.PLAIN, 16));
+		yesButton = new JButton(new ImageIcon(getClass().getClassLoader().getResource("images/yes_button.png")));
+		yesButton.setRolloverIcon(new ImageIcon(getClass().getClassLoader().getResource("images/yes_hoverButton.png")));
+		yesButton.setBounds(48, 114, 50, 30);
+		modifyButton(yesButton);
+		
 		noButton = new JButton("NO");
-		add(jLabel);
-		add(yesButton);
-		add(noButton);
+		noButton = new JButton(new ImageIcon(getClass().getClassLoader().getResource("images/no_button.png")));
+		noButton.setRolloverIcon(new ImageIcon(getClass().getClassLoader().getResource("images/no_hoverButton.png")));
+		noButton.setBounds(104, 114, 50, 30);
+		modifyButton(noButton);
+		
+		getContentPane().setLayout(null);
+		getContentPane().add(jLabel);
+		getContentPane().add(yesButton);
+		getContentPane().add(noButton);
 	}
 
+	public void modifyButton(JButton button) {
+		button.setOpaque(false);
+		button.setContentAreaFilled(false);
+		button.setBorderPainted(false);
+		button.setFocusable(false);
+	}
+	
 	public JButton getYesButton() {
 		return yesButton;
 	}
